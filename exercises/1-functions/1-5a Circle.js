d905ee292e11ae183d941aeeb0200988198b8d00
Joel Fr. López Méndez <joel.lopez.mendez@ehb.be>
2022-10-05 16:02:51 +0200

    Oefenig 5

--- exercises/1-functions/1-5a Circle.js
+++ exercises/1-functions/1-5a Circle.js
@@ -5,15 +5,29 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let context = canvas.getContext('2d');
 
+drawRect1();
+drawCicle();
 drawRect();
-drawLine();
 
-function drawRect() {
-    context.fillStyle = '#18F7EA';
-    context.strokeStyle = 'black';
+function drawRect1() {
+    context.fillStyle = '#FE8B00';
     context.lineWidth = '3';
     context.beginPath();
     context.rect(50, 50, 350, 350);
-    context.stroke();
+    context.fill();
+}
+
+function drawCicle() {
+    context.fillStyle = 'yellow';
+    context.beginPath();
+    context.arc(225, 300, 100, Math.PI, 0);
+    context.fill();
+}
+
+function drawRect() {
+    context.fillStyle = 'blue';
+    context.lineWidth = '3';
+    context.beginPath();
+    context.rect(50, 300, 350, 100);
     context.fill();
 }
