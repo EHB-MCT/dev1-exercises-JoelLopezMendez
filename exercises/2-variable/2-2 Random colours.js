8419f405cb7c66f5fa27db705d0611899c4520a8
Joel Fr. López Méndez <joel.lopez.mendez@ehb.be>
2022-10-12 16:02:07 +0200

    Random color finish

--- exercises/2-variable/2-2 Random colours.js
+++ exercises/2-variable/2-2 Random colours.js
@@ -8,18 +8,32 @@ let context = canvas.getContext('2d');
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-let margin = 50;
 
-drawRec(50, 50, margin + 500, margin + 500, Math.random() * 50);
-drawRec(75, 75, margin + 450, margin + 450, "#12F90E");
-drawRec(100, 100, margin + 400, margin + 400, "#0E43F9");
-drawRec(125, 125, margin + 350, margin + 350, "#5499C7");
-drawRec(150, 150, margin + 300, margin + 300, "#008C00");
-drawRec(175, 175, margin + 250, margin + 250, "#00538C");
 
-function drawRec(x, y, w, h, color) {
+/* drawRec(50, 50, size + 50, size + 50);
+drawRec(75, 75, size, size);
+drawRec(100, 100, size - 50, size - 50);
+drawRec(125, 125, size - 100, size - 100);
+drawRec(150, 150, size - 150, size - 150);
+drawRec(175, 175, size - 200, size - 200); */
+
+let size = 500;
+
+function drawRec(x, y, size) {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    let color = "rgb(" + r + "," + g + "," + b + ")";
+
     context.fillStyle = color;
-    context.beginPath();
-    context.rect(x, y, w, h);
-    context.fill();
+    context.fillRect(x, y, size, size);
 }
+
+drawRec(50, 50, size);
+drawRec(75, 75, size - 50);
+drawRec(100, 100, size - 100);
+drawRec(125, 125, size - 150);
+drawRec(150, 150, size - 200);
+drawRec(175, 175, size - 250);
+drawRec(200, 200, size - 300);
+drawRec(225, 225, size - 350);
